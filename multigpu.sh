01cb02e1229f72436c28ade2d9249fc19854b93b
Prakalp Srivastava <prakalps@octoml.ai>
2023-11-09 11:12:07 +0000

    Improve multigpu script

--- multigpu.sh
+++ multigpu.sh
@@ -1,7 +1,18 @@
-MODEL=CodeLlama-34b-Instruct-hf
-QUANT=q4f16_1
-
 SHARDS=2
+QUANT=q0f16
+
+while getopts q:s: flag
+do
+    case "${flag}" in
+        q) QUANT=${OPTARG};;
+        s) SHARDS=${OPTARG};;
+    esac
+done
+MODEL=CodeLlama-34b-Instruct-hf
+
+echo "Model: $MODEL";
+echo "Quant: $QUANT";
+echo "Shards: $SHARDS";
 
 echo "Clear existing build folder..."
 rm -rf ./dist/$MODEL-$QUANT/
@@ -12,9 +23,13 @@ python -m mlc_llm.build --model $MODEL --quantization $QUANT --num-shards $SHARD
 echo "Convert weights..."
 python -m mlc_llm.build --model $MODEL --quantization $QUANT --num-shards $SHARDS --max-seq-len 4096 --target cuda-multiarch --use-cuda-graph --use-flash-attn-mqa --convert-weight-only
 
-echo "Benchmarking..."
-python tests/benchmark.py --model $MODEL --quantization $QUANT --num-shards $SHARDS --num-warm-up=2 --num-measurements=5 --benchmark-mode=tvm --num-input-tokens=2000 --num-output-tokens=128
-
 echo "Storing config..."
 mv ./dist/$MODEL-$QUANT ./dist/$MODEL-s$SHARDS-$QUANT
+
+echo "Benchmarking..."
+echo "Model: $MODEL";
+echo "Quant: $QUANT";
+echo "Shards: $SHARDS";
+python tests/benchmark.py --model $MODEL --quantization $QUANT --num-shards $SHARDS --num-warm-up=2 --num-measurements=5 --benchmark-mode=tvm --num-input-tokens=2000 --num-output-tokens=128
+
 
